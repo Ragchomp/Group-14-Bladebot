e0295ed6000a4d7ccea28f365b06b77c4a66d705
StianFHatlestad <69855895+StianFHatlestad@users.noreply.github.com>
2023-05-26 15:43:44 +0200

    Del

--- Source/BladeBot/BladeBot.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BladeBot.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BladeBot, "BladeBot" );
