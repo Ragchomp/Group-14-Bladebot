234e9b45efe437510953fe8e32a8756f03fcfef9
Sebastian Zapart <259383@stud.inn.no>
2024-02-02 12:38:35 +0100

    added start, stop and rest function for time. Core of status zones complete, just need functionality

--- Source/BladeBot/Public/StatusZones/MainStatusZone.h
+++ Source/BladeBot/Public/StatusZones/MainStatusZone.h
@@ -19,4 +19,37 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	// VFX Functions
+	UFUNCTION(BlueprintCallable)
+	virtual void UpdateVFXLocationRotation();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void PlayVFXPassive(const FVector& PlayLocation);
+
+	// Audio Functions
+	UFUNCTION(BlueprintCallable)
+	virtual void PlayAudioPassive(const FVector& Location);
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere, Category = "VFX")
+	class UNiagaraSystem* PassiveVFX;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	class USoundBase* PassiveSound;
+	
+	UPROPERTY(EditAnywhere)
+	class UNiagaraComponent* NiagaraComp;
+
+	// Constants ------------------------------
+
+	bool alreadyHit = false;
+
 };
