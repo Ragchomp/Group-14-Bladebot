e16f063b638e810f05b920b3b606757c7c7c7bbc
Sebastian Zapart <259383@stud.inn.no>
2024-02-07 09:58:22 +0100

    deleted more code

--- Source/BladeBot/I_DamageEventContainer.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "I_DamageEventContainer.h"
-
-// Add default functionality here for any II_DamageEventContainer functions that are not pure virtual.
